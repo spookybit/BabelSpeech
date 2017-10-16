419801daa13069eeddfebdd4f3b3137c6e9e129d
Vincent Chan <vincent.chan@biola.edu>
2017-10-15 21:32:23 -0700

    setVoice

--- js/voice.js
+++ js/voice.js
@@ -8,10 +8,17 @@ msg.text = document.querySelector('[name="text"]').value;
 function populateVoices() {
   // console.log(this);
   voices = this.getVoices();
-  console.log(voices);
+  // console.log(voices);
   voicesDropdown.innerHTML = voices
+    // .filter(voice => voice.lang.includes('zh-CN'))
     .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
     .join('');
+  // console.log(voicesDropdown.innerHTML);
+}
+
+function setVoice() {
+  msg.voice = voices.find(voice => voice.name === this.value);
 }
 
 speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voicesDropdown.addEventListener('change', setVoice);
